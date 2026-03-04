b7f178a1d6a61a6412dd1df2cbd8d14cc42ec4f8
Niclas Skyttberg <nskyttberg@gmail.com>
2026-03-04 08:57:41 +0100

    fix: add Employee type to types.ts

---
+++ lib/types.ts
@@ -0,0 +1,10 @@
+﻿export interface Employee {
+  id: string;
+  name: string;
+  email: string;
+  site: string | null;
+  is_admin: boolean;
+  auth_user_id: string | null;
+  active: boolean;
+  created_at: string;
+}
